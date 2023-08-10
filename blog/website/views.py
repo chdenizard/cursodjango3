3cdb54ba0a126fb523cf951ffa42f0812cdbfbf8
Christopher Denizard <christopher@pop-os.localdomain>
2023-08-10 11:46:06 -0300

    apos acesso ao BD

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 def hello_blog(request):
     lista = ['Django','Python']
-    data = {'name': 'Curso de Django 3', 'lista_tecnologia': lista}
+    list_posts = Post.objects.all
+    data = {
+        'name': 'Curso de Django 3', 
+        'lista_tecnologia': lista, 
+        'posts' : list_posts}
     return render(request, 'index.html', data)
