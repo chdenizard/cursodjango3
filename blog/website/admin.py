d1837751964e1d36711afdb4eb899dae0128d1c3
Christopher Denizard <christopher@pop-os.localdomain>
2023-08-10 13:11:08 -0300

    pre-template

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,4 +1,15 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'categories', 'approved']
+    search_fields = ['title','sub_title']
+    
+    def get_queryset(self, request):
+        return Post.objects.filter(approved = True)
+    
+
+admin.site.register(Post, PostAdmin)
