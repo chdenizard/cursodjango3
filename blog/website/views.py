a96e57cda45421cf2e5f99b1ef512ad4e6287489
Christopher Denizard <christopher@pop-os.localdomain>
2023-08-10 11:30:16 -0300

    Aula de templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def hello_blog(request):
+    lista = ['Django','Python']
+    data = {'name': 'Curso de Django 3', 'lista_tecnologia': lista}
+    return render(request, 'index.html', data)
